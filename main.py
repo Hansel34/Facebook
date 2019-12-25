487024c98aa43b098fd97a2230336c1b25799009
Hansel <qiu_hansel@hotmail.com>
2019-12-24 23:30:21 -0800

    rank individual non-group convos

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import json
 import os
 from collections import defaultdict
 
+# class message_class:
+# 	def __init__(self):
+# 		self.participants = None
+# 		self.wordOccurrence = defaultdict(lambda: defaultdict(int))
+# 		self.messageOccurrence = defaultdict(lambda: defaultdict(int))
+
 class message_class:
 	def __init__(self):
 		self.participants = None
-		self.wordOccurrence = defaultdict(lambda: defaultdict(int))
-		self.messageOccurrence = defaultdict(lambda: defaultdict(int))
+		self.wordOccurrence = defaultdict(int)
+		self.messageOccurrence = defaultdict(int)
 		
 def analyzeFile(message_file,message_data):
 	with open(message_file) as data_file:
@@ -33,12 +39,40 @@ def forPerson(dir):
 		for time in message_data.messageOccurrence[key]:
 			output_file.write(str(time)+','+str(message_data.messageOccurrence[key][time])+','+str(message_data.wordOccurrence[key][time])+'\n')
 
+participants = defaultdict(message_class)
+
+def group(dir):
+	with open(dir) as data_file:
+		data = data_file.read()
+		data = json.loads(data)
+		key = []
+		if len(data['participants']) > 2:
+			return
+		for participant in data['participants']:
+			key.append(participant["name"])
+		key = ",".join(key)
+		for message in data['messages']:
+			participants[key].messageOccurrence[message['sender_name']] += 1
+			if 'content' in message:
+				participants[key].wordOccurrence[message['sender_name']] += len(message['content'].split())
 
 def traverseFolder():
 	for path, sub_path, file_names in  os.walk("/mnt/c/Users/black/Downloads/messages/"):
 		for file in file_names:
 			if file.endswith(".json"):
-				print(file)
+				group(os.path.join(path,file))
+	#output_file = open(dir_folder + '/' +'output.csv','w')
+	output_list = []
+	for key in participants:
+		total = 0
+		output_string = ""
+		for person in participants[key].wordOccurrence:
+			total += participants[key].wordOccurrence[person]
+			output_string += person + ',' + str(participants[key].wordOccurrence[person]) + ","
+		output_list.append([total,output_string])
+	output_list.sort(reverse = True)
+	for output in output_list:
+		print(output[0], ",", output[1])
 
 traverseFolder()
 
